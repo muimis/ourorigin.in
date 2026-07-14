bef4ff577efbc3f4514d162468c623b46904ec2c
OurOrigin Engineering <engineering@ourorigin.com>
2026-07-14 12:48:46 +0530

    feat: expand product catalog, add utility constants, and implement new site components and pages

---
+++ src/utils/constants.ts
@@ -0,0 +1,8 @@
+// src/utils/constants.ts
+
+export const WHATSAPP_NUMBER = '919645555668';
+export const WHATSAPP_LINK = `https://wa.me/${WHATSAPP_NUMBER}`;
+export const EMAIL_ADDRESS = 'ourorigin.in@gmail.com';
+export const FSSAI_NUMBER = '21326135000354';
+export const CANONICAL_DOMAIN = 'ourorigin.in';
+export const SITE_URL = `https://${CANONICAL_DOMAIN}`;
