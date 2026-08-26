a9d840c3e9386d72948d8e693afd92ca3ba929cc
OurOrigin Engineering <engineering@ourorigin.com>
2026-08-26 12:36:08 +0530

    feat: implement core site framework with landing page, terms, ledger, and product data structures

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -6,3 +6,4 @@ export const EMAIL_ADDRESS = 'ourorigin.in@gmail.com';
 export const FSSAI_NUMBER = '21326135000354';
 export const CANONICAL_DOMAIN = 'ourorigin.in';
 export const SITE_URL = `https://${CANONICAL_DOMAIN}`;
+export const GOOGLE_SHEET_API_URL = 'https://script.google.com/macros/s/AKfycbxqCze6nUXRoz4_S7wUpamp3BGyM2Dzm_HUHjRcwBDee1eeowaDgVnGAtGdn7lrjbMl/exec';
